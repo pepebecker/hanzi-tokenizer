4b8f1813040294dfcbac05b54e6635ff7cc1b678
Pepe Becker <mail@pepebecker.com>
2018-11-14 10:38:41 +0900

    Update test description

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 const getTokens = require('../index')
 
 describe('Get Tokens', () => {
-	it('should convert characters to marked pinyin', () => {
+	it('should convert string of characters to list of character data', () => {
 		return getTokens('我的猫喜欢喝牛奶')
 		.then(tokens => {
 			tokens.map(token => token.traditional)
